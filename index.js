7f8972fd23b00b7141ee9a27ad94f5265df8d6a3
Alice Breton <aliceb@bam.tech>
2017-10-19 14:43:10 +0200

    Export action type

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import { NetInfo } from "react-native";
 
+export const STATUS_CHANGED = "STATUS_CHANGED";
+
 export default () => createStore => (...args) => {
   const store = createStore(...args);
 
   const checkConnection = isConnected => {
     store.dispatch({
-      type: "STATUS_CHANGED",
+      type: STATUS_CHANGED,
       payload: { isConnected }
     });
   };
